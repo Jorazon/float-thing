7d84a97ffe81ad6b1d6e326acabc7a9afb3eef65
Jorazon <oskari.pahkala@gmail.com>
2025-01-24 17:48:45 +0200

    Update flaot.c

--- flaot.c
+++ flaot.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
+#include <stdint.h>
 
 typedef union {
     struct {
-        unsigned int frac:23;
-        unsigned int expt:8;
-        unsigned int sign:1;
+        uint32_t frac:23;
+        uint32_t expt:8;
+        uint32_t sign:1;
     };
     float value;
 } float_union;
 
 typedef struct {
-    unsigned long long int integer;
-    unsigned long long int decimal;
+    uint64_t integer;
+    uint64_t decimal;
 } float_llu;
 
 float_llu parse(float value) {
-    float_union bruh;
-    bruh.value = value;
+    float_union unified;
+    unified.value = value;
 
-    int sign = bruh.sign?-1:1;
-    int expt = bruh.expt - 127;
-    unsigned long long int frac = bruh.frac | 1<<23;
+    int sign = unified.sign?-1:1;
+    int expt = unified.expt - 127;
+    int frac = unified.frac | 1<<23;
+    
+    printf("%llu %lld %llu\n", sign, expt, frac);
+    printf("%llu %lld %llu\n", sign, expt, frac);
 
     // denormalize numbers < 1
     if (expt < 0) frac >>= -expt;
 
-    unsigned long long int sum = 0;
-    unsigned long long int e = 10;
+    uint64_t sum = 0;
+    uint64_t e = 10;
 
+    // >18 causes integer overflow, results in imprecision
     for (int i = 0; i < 18; i++) {
-        int bit = ((frac >> ((23 - 1) - i)) & 1);
-        int two_pow_i = (2 << i);
-        //printf("%llu %d %llu\n", sum, bit, e / two_pow_i);
+        if ((frac << i) == 0) break;
+        char bit = ((frac >> ((23 - 1) - i)) & 1);
+        uint32_t two_pow_i = (2 << i);
+        printf("%llu %d %llu\n", sum, bit, e / two_pow_i);
         if (bit) sum += e / two_pow_i;
         sum *= 10;
         e *= 10;
@@ -41,6 +47,8 @@ float_llu parse(float value) {
 }
 
 void main(void) {
-    float_llu a = parse(1.1);
+    int x = 0b00111110101010101010101010000000;
+    float_llu a = parse(*(float*)&x);
     printf("%llu.%llu\n", a.integer, a.decimal);
+    //0.333332061767578125
 }
